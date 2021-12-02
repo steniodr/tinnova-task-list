267cd8b0460e56748310ec0d071eadfe4d390275
Stenio D. Rapchan <steniodr@hotmail.com>
2021-12-02 11:14:09 -0300

    add: task 03 Done

---
+++ task 03/index.js
@@ -0,0 +1,15 @@
+/*
+    To test this code, pass the desired number as an argument.
+    Example: node index.js 5
+*/
+
+let number = process.argv[2] || 1;
+let resul = number;
+
+if (number == 1 || number == 0) return console.log('Tente novamente com outro numero...');
+
+for(let i = number - 1; i > 1; i-- ){
+    resul *= i;
+}
+
+console.log(`${number}! = ${resul}`);
