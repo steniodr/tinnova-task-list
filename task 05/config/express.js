2c891dd65da825ef3d84bd47ad4e2f665b61c3e5
Stenio D. Rapchan <steniodr@hotmail.com>
2021-12-13 19:28:09 -0300

    fix: clean some code

--- task 05/config/express.js
+++ task 05/config/express.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const consign = require('consign');
 
-
-
 module.exports = () => {
     const app = express();
     app.use(express.urlencoded ({ extended: false }));
--- task 05/server.js
+++ task 05/server.js
@@ -4,24 +4,3 @@ const port = app.get('port');
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 });
-
-
-
-// function test(data) {
-//     const valid = validate(data)
-//     if (valid) console.log("Valid!")
-//     else console.log("Invalid: " + ajv.errorsText(validate.errors))
-// }
-
-// const validate = ajv.compile(schema)
-
-// test({
-//     veiculo: "Argo",
-//     marca: "Fiat",
-//     ano: 2020,
-//     descricao: "Carro tá potente",
-//     vendido: false,
-// })
-
-
-
